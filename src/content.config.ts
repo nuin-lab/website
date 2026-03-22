51fda6a5d710302bac5ba94197b20d123cbf1c3a
nuin <62213079+nui-n@users.noreply.github.com>
2026-03-22 17:12:34 +0900

    *: ライセンスヘッダーの追加

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 nuin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 
@@ -6,7 +22,10 @@ import { ArticleFrontmatterSchema } from './lib/schemas/article.schema';
 
 const articles = defineCollection({
   // Load Markdown files in the `src/content/articles/` directory.
-  loader: glob({ base: './src/content/articles', pattern: '**/*.md' }),
+  loader: glob({
+    base: './src/content/articles',
+    pattern: '**/*.md',
+  }),
   // Type-check frontmatter using a schema
   schema: () => ArticleFrontmatterSchema,
 });
